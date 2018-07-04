b5bea1a4b7a8d0c78081bd6fbe7e0cedc1004d94
Vipin V <vipin.v_world@hotmail.com>
2018-07-05 00:02:13 +0530

    sample Api call
    - bug pending

--- app/src/main/java/arch/line/base/baseline/repository/api/ImdbApi.java
+++ app/src/main/java/arch/line/base/baseline/repository/api/ImdbApi.java
@@ -1,4 +1,13 @@
 package arch.line.base.baseline.repository.api;
 
+import android.arch.lifecycle.LiveData;
+
+import arch.line.base.baseline.model.ExampleDataModel;
+import arch.line.base.baseline.utility.api.ApiResponse;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface ImdbApi {
+    @GET("")
+    public LiveData<ApiResponse<ExampleDataModel>> getUser(@Query("apikey") String apiKey, @Query("i") String id);
 }
